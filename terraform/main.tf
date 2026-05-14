feaed7b64ef73aa32e1c7ecfacf8a619518e7ad2
Sambhav Singh <sambhavsingh8034@gmail.com>
2026-05-14 23:39:38 +0530

    terraform added

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,46 +29,12 @@ data "aws_ami" "ubuntu" {
 }
 
 # Security Group
-resource "aws_security_group" "k8s_sg" {
-  name        = "k8s-security-group"
-  description = "Allow SSH, 3000 and 5000"
+# USE EXISTING SECURITY GROUP
+data "aws_security_group" "k8s_sg" {
+
+  name   = "k8s-security-group"
 
   vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Port 3000"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Port 5000"
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "k8s-security-group"
-  }
 }
 
 # EC2 Instance
@@ -77,7 +43,7 @@ resource "aws_instance" "k8s_ec2" {
   instance_type          = "m7i-flex.large"
   key_name               = var.key_name
   subnet_id              = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.k8s_sg.id]
 
   associate_public_ip_address = true
 
